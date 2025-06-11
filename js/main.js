8db5992c8209dec9872727f2299d7c5026b3522b
nschuelke1 <ndschuelke@wisc.edu>
2025-06-11 17:44:07 -0500

    turf and new API

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
     ], function(esriConfig, Map, MapView, Search, FeatureLayer) {
 
         // ArcGIS Online API Key
-        esriConfig.apiKey = "AAPTxy8BH1VEsoebNVZXo8HurA_2jA8sPPf_DuV7jRLl5PwtnXSU0EiBd11SD4M-Bxw0ijY9Rnbq1CTi5xlxNyFji8DGRu0KKLCDqwHVksc_cd45A11yCLF6IS0qeLbPPoLAz_Lb-HlU59LPiRtjGTm8JRkfTU1zLRk6lnCjPZw0qLurNObS9cuNtbfmuGah_zEHj40iGyODgqADtCiqHiuUVKOV6bBXkVGF9GQp2qF_X_KoecZRYMv2u3hz_XOz0WmGAT1_ZrUwUfy9"; // 🔥 Replace with your actual key
+        esriConfig.apiKey = "AAPTxy8BH1VEsoebNVZXo8HurA_2jA8sPPf_DuV7jRLl5PwtnXSU0EiBd11SD4M-Bxw0SL6XWQxKmE1p3BlBtJG9VJ-nY77ywrxirQx3WGMNy6ErMjdE3Nj8n2_6fpjVyz77akfVF7g3G-b238insDrWtOVvtfYbyTKoFa7pO48S05clji-hthN9rMEaqmA4FE-6A5sUrnTvvt95VOFSnyV9eNgP75OZbv1em2a4O2YPBfVGuHO763jHFZ50R9R_88hbAT1_ZrUwUfy9"; 
 
         // Initialize the Map
         var map = new Map({
