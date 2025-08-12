4c921b0ef59b1cfa9013342f67782e95723a19eb
nschuelke1 <ndschuelke@wisc.edu>
2025-08-12 14:38:22 -0500

    updated server.js

--- js/server.js
+++ js/server.js
@@ -3,12 +3,12 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files
-app.use(express.static(path.join(__dirname, "public"))); // or just "." if everything is in root
+// Serve static files from the project root
+app.use(express.static(path.join(__dirname, "..")));
 
-// Fallback to index.html
+// Fallback to index.html in the root
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "../index.html"));
 });
 
 app.listen(PORT, () => {
