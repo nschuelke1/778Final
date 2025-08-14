832b691c8d0414bb8e4073cba7963707e3136c0d
nschuelke1 <ndschuelke@wisc.edu>
2025-08-14 16:51:24 -0500

    server.js

--- js/server.js
+++ js/server.js
@@ -14,3 +14,8 @@ app.get("*", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use((req, res, next) => {
+  console.log(`Request: ${req.method} ${req.url}`);
+  next();
+});
